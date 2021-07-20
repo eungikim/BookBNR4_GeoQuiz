bfacb39bcc1e13b3d7814f439fbea833e7cc094d
eungikim <eungikim.me@gmail.com>
2021-07-20 16:13:56 +0900

    Add anim when activity change

--- app/src/main/java/me/eungi/geoquiz/MainActivity.kt
+++ app/src/main/java/me/eungi/geoquiz/MainActivity.kt
@@ -1,7 +1,9 @@
 package me.eungi.geoquiz
 
 import android.app.Activity
+import android.app.ActivityOptions
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.Gravity
@@ -70,7 +72,13 @@ class MainActivity : AppCompatActivity() {
         cheatButton.setOnClickListener {
             val answerIsTrue = quizViewModel.currentQuestionAnswer
             val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
-            startActivityForResult(intent, REQUEST_CODE_CHEAT)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val optionAnim =
+                    ActivityOptions.makeClipRevealAnimation(it, 0, 0, it.width, it.height)
+                startActivityForResult(intent, REQUEST_CODE_CHEAT, optionAnim.toBundle())
+            } else {
+                startActivityForResult(intent, REQUEST_CODE_CHEAT)
+            }
         }
 
         updateQuestion()
